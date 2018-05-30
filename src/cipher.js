d64a58e11f92c0831eec506bb289e67d23e5dbcb
lorenareyes <lorenacrisreyes@gmail.com>
2018-05-30 08:24:30 -0500

    invalido

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,16 @@
 window.cipher = {
   // ... 
+  encode: (offset, string) => {
+
+    
+
+    for (let i = 0; i<str.length; i++) {
+
+       
+    }
+
+  },
+  decode: (offset, string) => {
+
+  }
 };
---
+++ src/index.js
@@ -0,0 +1,5 @@
+
+const offsetStr = document.getElementById('offset').value+1;
+const offset= parseInt(offsetStr);
+const newOffset = str.charCodeAt(offset);
+const texto = document.getElementById('texto');
